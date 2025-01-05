38f2a79f28d3e3fb6085e7ccd8afccfaf0878a81
Angelo Ascione <a.ascione@asignt.it>
2025-01-05 19:28:29 +0100

    test split

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import textutils
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 def print_initial_info(dataset):
     # print(dataset.head())
@@ -21,15 +22,24 @@ def normalize_data(dataset):
     return dataset
 
 def main():
-    dataset = pd.read_csv("./dataset/SuicideAndDepression_Detection.csv")
-    dataset = clean_data(dataset)
-    dataset = normalize_data(dataset)
-    dataset = clean_data(dataset)
-    encoder = LabelEncoder()
-    dataset['class'] = encoder.fit_transform(dataset['class'])
-    dataset.to_csv('./dataset/cleaned_dataset.csv', index=False)
+    # dataset = pd.read_csv("./dataset/SuicideAndDepression_Detection.csv")
+    # dataset = clean_data(dataset)
+    # dataset = normalize_data(dataset)
+    # dataset = clean_data(dataset)
+    # encoder = LabelEncoder()
+    # dataset['class'] = encoder.fit_transform(dataset['class'])
+    # dataset.to_csv('./dataset/cleaned_dataset.csv', index=False)
 
-    print(encoder.classes_)
+    dataset = pd.read_csv("./dataset/cleaned_dataset.csv")
+    X = dataset['text']
+    y = dataset['class']
+
+    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+
+    print(f"Training set: {len(X_train)}")
+    print(f"Validation set: {len(X_val)}")
+    print(f"Test set: {len(X_test)}")
 
 
 if __name__ == "__main__":
